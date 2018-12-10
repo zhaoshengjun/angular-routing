738669f24f7f2b6cb21faba91ceb86c7071ca163
Sam Zhao <zhao.shengjun@gmail.com>
2018-12-10 22:32:08 +1000

    show parameter in html

--- src/app/people/person-detail.component.ts
+++ src/app/people/person-detail.component.ts
@@ -7,11 +7,13 @@ import { ActivatedRoute } from "@angular/router";
   styleUrls: ["./person-detail.component.css"]
 })
 export class PersonDetailComponent implements OnInit {
+  personId: string;
   constructor(private activeRoute: ActivatedRoute) {}
 
   ngOnInit() {
     this.activeRoute.params.subscribe(data => {
-      console.log("route id", data);
+      console.log(data);
+      this.personId = data["personeId"];
     });
   }
 }
