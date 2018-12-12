fa165eae85a2e3c9681028dc9db4fcf552f53782
Sam Zhao <zhao.shengjun@gmail.com>
2018-12-12 21:30:21 +1000

    add service

---
+++ src/app/people/people.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@angular/core";
+import { of } from "rxjs";
+const people = [{ id: 1, name: "John" }, { id: 2, name: "Jane" }];
+@Injectable({
+  providedIn: "root"
+})
+export class PeopleService {
+  constructor() {}
+
+  getPersonById(personId: number) {
+    return of(people.filter(x => x.id === personId));
+  }
+}
